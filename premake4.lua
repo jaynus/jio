4b85fa81d8da9272b68c343e46b7b4b25fe6508b
Walter Pearce <wpearce@corelogic.com>
2014-10-08 15:39:26 -0700

    save

---
+++ premake4.lua
@@ -0,0 +1,44 @@
+solution("CryGame")
+	location("solutions/vcproj" .. _ACTION)
+	platforms { "x32", "x64" }
+
+	language("C++")
+
+	configurations{"Debug", "Release"}
+
+	project("jio")
+		location("solutions/" .. _ACTION)
+
+		flags("NoMinimalRebuild")
+
+		buildoptions
+		{
+			"/Gy",
+			"/GS",
+			"/GF",
+			"/MP",
+			"/arch:SSE",
+		}
+
+		language("C++")
+		kind("Exe")
+
+		targetdir("game/bin32/")
+		targetname("CryGameSDK")
+
+		objdir("obj/")
+
+		includedirs(".")
+		includedirs("external/protobuf-2.6.0/src")
+		includedirs("test")
+		
+		files("**.hpp")
+		files("**.hpp")
+		files("**.hpp")
+		
+
+		configuration("Debug")
+			flags("Symbols")
+
+		configuration("Release")
+			flags("Optimize")
