b598e5192881accab2e12e9e1f9f7ea1050fe088
Walter Pearce <wpearce@corelogic.com>
2014-10-10 10:28:13 -0700

    - ts3 bugfix and iterator fix
    - on-purpose off-by-one

--- transports/teamspeak3.hpp
+++ transports/teamspeak3.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
-/*!
-* Named pipe implementation class.
-*/
+
 #include <jio/xplatform.hpp>
 #include <jio/exception.hpp>
 #include <jio/transports/common.hpp>
@@ -16,6 +14,9 @@ namespace jio {
 	namespace transports {
 		using namespace jio::messaging;
 
+		/*!
+		* Teamspeak 3 transport implementation class for ACRE.
+		*/
 		class teamspeak3 : public base_transport {
 		public:
 			teamspeak3() { }
@@ -47,6 +48,9 @@ namespace jio {
 
 				this->_queue_in.push(read_message);
 			}
+			/*!
+			* TS3 implementation middle-man function for calling into API with sendPLuginCommand
+			*/
 			uint32_t impl_sendPluginCommand(const unsigned char *buffer, int target){
 				//ts3Functions.sendPluginCommand(serverConnectionHandlerID, pluginID, buffer, target, NULL, NULL);
 				
@@ -79,33 +83,39 @@ namespace jio {
 			message *  read(i_message_factory *factory) { THROW_NOT_IMPL(); return nullptr; }
 			message *  read(message & msg) { THROW_NOT_IMPL(); return nullptr; }
 
+			/*!
+			*	Sends a message object via TS3. If they message data contains nulls, it is base64 encoded for 
+			*	TS3 string handling. If it does not, its sent plain over the wire. Overhead of a NULL containing
+			*	message is an extra allocation, base64 encode, and deallocation. Sending a non-null message
+			*	will just temporarily null-terminate the buffer (and off-by-one on purpose) temporarily for performance.
+			*/
 			uint32_t		write(const message & msg) { 
 				char *encoded_message = nullptr;
 				unsigned char *search_ptr = nullptr;
+				unsigned char save_char;
 				size_t encoded_length = 0;
-
+			
 				// break if we hit a null
 				for (search_ptr = msg.data; search_ptr != (msg.data + msg.length); search_ptr++) {
-					if (search_ptr == 0x00)
+					if (*search_ptr == 0x00)
 						break;
 				}
 
 				// if we hit the end of the stream and there were no nulls, send raw.
 				if (search_ptr == (msg.data + msg.length)) {
-							
-					// No nulls, null terminate it and send it
-					encoded_message = new char[msg.length + 1];
-					memcpy(encoded_message, msg.data, msg.length);
-					encoded_message[msg.length] = 0x00;
-
+					// null-terminate and save the data object, temporary for sending
+					if (*search_ptr != 0x00) {
+						save_char = *search_ptr;
+						*search_ptr = 0x00;
+					}
+					impl_sendPluginCommand(msg.data, ts3_target);
+					*search_ptr = save_char;
 				} else { // There were nulls, encode.
-					
 					//base64 encode it
 					encoded_message = _base64.encode(msg.data, msg.length, &encoded_length);
+					impl_sendPluginCommand(msg.data, ts3_target);
+					delete encoded_message;
 				}
-				uint32_t ret = impl_sendPluginCommand(msg.data, ts3_target);
-				
-				delete encoded_message;
 
 				return msg.length;
 			}
